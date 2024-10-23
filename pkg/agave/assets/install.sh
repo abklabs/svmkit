be012aff7fafff7be0a59af0ecd2457b467125a1
Alexander Guy <alexander.guy@pm.me>
2024-10-23 14:18:11 -0700

    shfmt

--- pkg/agave/assets/install.sh
+++ pkg/agave/assets/install.sh
@@ -2,26 +2,26 @@
 # shellcheck shell=bash
 
 step::00::setup-abklabs-apt() {
-  apt::abk
+    apt::abk
 }
 
 step::10::install-base-software() {
-  $SUDO apt-get update
-  $APT install logrotate ufw
+    $SUDO apt-get update
+    $APT install logrotate ufw
 }
 
 step::20::create-sol-user() {
-  id sol >/dev/null 2>&1 || $SUDO adduser --disabled-password --gecos "" sol
-  $SUDO mkdir -p "/home/sol"
-  $SUDO chown -f -R sol:sol "/home/sol"
+    id sol >/dev/null 2>&1 || $SUDO adduser --disabled-password --gecos "" sol
+    $SUDO mkdir -p "/home/sol"
+    $SUDO chown -f -R sol:sol "/home/sol"
 }
 
 step::30::copy-validator-keys() {
-  keypairs::write "sol" "${IDENTITY_KEYPAIR}:validator-keypair" "${VOTE_ACCOUNT_KEYPAIR}:vote-account-keypair"
+    keypairs::write "sol" "${IDENTITY_KEYPAIR}:validator-keypair" "${VOTE_ACCOUNT_KEYPAIR}:vote-account-keypair"
 }
 
 step::40::configure-sysctl() {
-  cat <<EOF | $SUDO tee /etc/sysctl.d/21-solana-validator.conf >/dev/null
+    cat <<EOF | $SUDO tee /etc/sysctl.d/21-solana-validator.conf >/dev/null
 # Increase UDP buffer sizes
 net.core.rmem_default = 134217728
 net.core.rmem_max = 134217728
@@ -34,22 +34,22 @@ fs.nr_open = 1000000
 vm.swappiness=1
 EOF
 
-  $SUDO sysctl -p /etc/sysctl.d/21-solana-validator.conf
+    $SUDO sysctl -p /etc/sysctl.d/21-solana-validator.conf
 }
 
 step::50::configure-firewall() {
-  $SUDO ufw allow 53
-  $SUDO ufw allow ssh
-  $SUDO ufw allow 8000:8020/tcp
-  $SUDO ufw allow 8000:8020/udp
-  # TODO: Only open for RPC nodes
-  $SUDO ufw allow 8899/tcp
-  $SUDO ufw allow 8899/udp
-  $SUDO ufw --force enable
+    $SUDO ufw allow 53
+    $SUDO ufw allow ssh
+    $SUDO ufw allow 8000:8020/tcp
+    $SUDO ufw allow 8000:8020/udp
+    # TODO: Only open for RPC nodes
+    $SUDO ufw allow 8899/tcp
+    $SUDO ufw allow 8899/udp
+    $SUDO ufw --force enable
 }
 
 step::60::setup-logrotate() {
-  cat <<EOF | $SUDO tee /etc/logrotate.d/solana >/dev/null
+    cat <<EOF | $SUDO tee /etc/logrotate.d/solana >/dev/null
 /home/sol/solana-validator.log {
 su sol sol
 daily
@@ -61,32 +61,32 @@ endscript
 }
 EOF
 
-  $SUDO systemctl restart logrotate
+    $SUDO systemctl restart logrotate
 }
 
 step::70::install-validator() {
-  if [[ -v VALIDATOR_VERSION ]]; then
-    $APT --allow-downgrades install "zuma-agave-validator=$VALIDATOR_VERSION" "zuma-solana-cli=$VALIDATOR_VERSION"
-  else
-    $APT --allow-downgrades install "zuma-agave-validator" "zuma-solana-cli"
-  fi
+    if [[ -v VALIDATOR_VERSION ]]; then
+        $APT --allow-downgrades install "zuma-agave-validator=$VALIDATOR_VERSION" "zuma-solana-cli=$VALIDATOR_VERSION"
+    else
+        $APT --allow-downgrades install "zuma-agave-validator" "zuma-solana-cli"
+    fi
 }
 
 step::80::setup-validator-startup() {
-  if systemctl list-unit-files svmkit-agave-validator.service >/dev/null; then
-    $SUDO systemctl stop svmkit-agave-validator.service || true
-  fi
+    if systemctl list-unit-files svmkit-agave-validator.service >/dev/null; then
+        $SUDO systemctl stop svmkit-agave-validator.service || true
+    fi
 
-  cat <<EOF | $SUDO tee /home/sol/run-validator >/dev/null
+    cat <<EOF | $SUDO tee /home/sol/run-validator >/dev/null
 #!/usr/bin/env bash
 
 $VALIDATOR_ENV exec agave-validator $VALIDATOR_FLAGS
 EOF
 
-  $SUDO chmod 755 /home/sol/run-validator
-  $SUDO chown sol:sol /home/sol/run-validator
+    $SUDO chmod 755 /home/sol/run-validator
+    $SUDO chown sol:sol /home/sol/run-validator
 
-  cat <<EOF | $SUDO tee /etc/systemd/system/svmkit-agave-validator.service >/dev/null
+    cat <<EOF | $SUDO tee /etc/systemd/system/svmkit-agave-validator.service >/dev/null
 [Unit]
 Description=SVMkit Agave validator
 
@@ -99,7 +99,7 @@ LimitNOFILE=1000000
 [Install]
 WantedBy=default.target
 EOF
-  $SUDO systemctl daemon-reload
-  $SUDO systemctl enable svmkit-agave-validator.service
-  $SUDO systemctl start svmkit-agave-validator.service
+    $SUDO systemctl daemon-reload
+    $SUDO systemctl enable svmkit-agave-validator.service
+    $SUDO systemctl start svmkit-agave-validator.service
 }
--- pkg/runner/assets/lib.bash
+++ pkg/runner/assets/lib.bash
@@ -1,11 +1,11 @@
 if [ -z "$BASH_VERSION" ]; then
-  echo "FATAL: This library requires bash to function properly!"
-  exit 1
+    echo "FATAL: This library requires bash to function properly!"
+    exit 1
 fi
 
 if [[ ${BASH_VERSINFO[0]} -lt 4 ]]; then
-  echo "FATAL: This library requires bash v4 or greater!"
-  exit 1
+    echo "FATAL: This library requires bash v4 or greater!"
+    exit 1
 fi
 
 : "${SUDO:=sudo --preserve-env=DEBIAN_FRONTEND}"
@@ -18,31 +18,31 @@ export DEBIAN_FRONTEND
 EXIT_FUNCS=()
 
 exit::trap() {
-  local func
-  for func in "${EXIT_FUNCS[@]}"; do
-    $func
-  done
+    local func
+    for func in "${EXIT_FUNCS[@]}"; do
+        $func
+    done
 }
 
 trap exit::trap EXIT
 
 exit::trigger() {
-  EXIT_FUNCS+=("$*")
+    EXIT_FUNCS+=("$*")
 }
 
 TMPDIR=$(mktemp -d)
 export TMPDIR
 
 temp::cleanup() {
-  log::debug cleaning up "$TMPDIR"...
-  rm -rf "$TMPDIR"
+    log::debug cleaning up "$TMPDIR"...
+    rm -rf "$TMPDIR"
 }
 
 exit::trigger temp::cleanup
 
 # shellcheck disable=SC2120 # these options are optional.
 temp::file() {
-  mktemp -p "$TMPDIR" "$@"
+    mktemp -p "$TMPDIR" "$@"
 }
 
 CRED=''
@@ -52,134 +52,134 @@ CBLU=''
 CNONE=''
 
 if [[ -t 1 ]]; then
-  CRED='\033[0;31m'
-  CGRN='\033[0;32m'
-  CYEL='\033[0;32m'
-  CBLU='\033[0;34m'
-  CNONE='\033[0m'
+    CRED='\033[0;31m'
+    CGRN='\033[0;32m'
+    CYEL='\033[0;32m'
+    CBLU='\033[0;34m'
+    CNONE='\033[0m'
 fi
 
 log::output() {
-  local level
-  level="$1"
-  shift
+    local level
+    level="$1"
+    shift
 
-  printf "$level:\t%s\n" "$*" >&2
+    printf "$level:\t%s\n" "$*" >&2
 }
 
 log::debug() {
-  [[ -v DEBUG ]] || return 0
+    [[ -v DEBUG ]] || return 0
 
-  log::output "${CBLU}DEBUG${CNONE}" "$@"
+    log::output "${CBLU}DEBUG${CNONE}" "$@"
 }
 
 log::info() {
-  log::output "${CGRN}INFO${CNONE}" "$@"
+    log::output "${CGRN}INFO${CNONE}" "$@"
 }
 
 log::warn() {
-  log::output "${CYEL}WARN${CNONE}" "$@"
+    log::output "${CYEL}WARN${CNONE}" "$@"
 }
 
 log::error() {
-  log::output "${CRED}ERROR${CNONE}" "$@"
+    log::output "${CRED}ERROR${CNONE}" "$@"
 }
 
 log::fatal() {
-  log::output "${CRED}FATAL${CNONE}" "$@"
-  exit 1
+    log::output "${CRED}FATAL${CNONE}" "$@"
+    exit 1
 }
 
 steps::run() {
-  local prefix start name
+    local prefix start name
 
-  prefix=$1
-  shift
-
-  start=""
-
-  if [[ $# -gt 0 ]]; then
-    start="${prefix}::$1"
+    prefix=$1
     shift
-    log::warn "starting steps with $start..."
-  fi
 
-  while read -r name; do
-    if [[ $name > $start || $name = "$start" ]]; then
-      log::info "====> running $name..."
-      $name
+    start=""
+
+    if [[ $# -gt 0 ]]; then
+        start="${prefix}::$1"
+        shift
+        log::warn "starting steps with $start..."
     fi
-  done < <(declare -F | grep "$prefix::" | awk '{ print $3; }')
+
+    while read -r name; do
+        if [[ $name > $start || $name = "$start" ]]; then
+            log::info "====> running $name..."
+            $name
+        fi
+    done < <(declare -F | grep "$prefix::" | awk '{ print $3; }')
 }
 
 ssh::config-file() {
-  local filename
-  filename="$(temp::file)"
-  cat >"$filename" <<EOF
+    local filename
+    filename="$(temp::file)"
+    cat >"$filename" <<EOF
 # This is fine, because we're using public key crypto for our access.
 LogLevel ERROR
 UpdateHostKeys no
 UserKnownHostsFile /dev/null
 StrictHostKeyChecking off
 EOF
-  echo "$filename"
+    echo "$filename"
 }
 
 git::version() {
-  git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD
+    git describe --tags --dirty 2>/dev/null || git rev-parse --short HEAD
 }
 
 env::kv() {
-  for varname in "$@"; do
-    if [[ -v $varname ]]; then
-      printf "%s='%s'\n" "$varname" "${!varname}"
-    fi
-  done
+    for varname in "$@"; do
+        if [[ -v $varname ]]; then
+            printf "%s='%s'\n" "$varname" "${!varname}"
+        fi
+    done
 }
 
 env::write() {
-  local var fname mode
+    local var fname mode
 
-  varname=$1
-  shift
-  fname=$1
-  shift
+    varname=$1
+    shift
+    fname=$1
+    shift
 
-  if [[ -v $varname ]]; then
-    touch "$fname"
+    if [[ -v $varname ]]; then
+        touch "$fname"
 
-    if [[ $# -gt 0 ]]; then
-      mode=$1
-      shift
-      chmod "$mode" "$fname"
+        if [[ $# -gt 0 ]]; then
+            mode=$1
+            shift
+            chmod "$mode" "$fname"
+        fi
+
+        printf "%s" "${!varname}" >"$fname"
     fi
-
-    printf "%s" "${!varname}" >"$fname"
-  fi
 }
 
 keypairs::write() {
-  local user="$1"
-  shift
-  local keypairs=("$@")
+    local user="$1"
+    shift
+    local keypairs=("$@")
 
-  for keypair in "${keypairs[@]}"; do
-    IFS=':' read -r content filename <<<"$keypair"
-    filename="${filename}.json"
-    log::info "Writing keypair to /home/$user/$filename"
-    sudo rm -f "/home/$user/$filename"
-    echo "$content" | sudo tee "/home/$user/$filename" >/dev/null
-    sudo chown "$user:$user" "/home/$user/$filename"
-    log::info "Keypair written and ownership set for /home/$user/$filename"
-  done
+    for keypair in "${keypairs[@]}"; do
+        IFS=':' read -r content filename <<<"$keypair"
+        filename="${filename}.json"
+        log::info "Writing keypair to /home/$user/$filename"
+        sudo rm -f "/home/$user/$filename"
+        echo "$content" | sudo tee "/home/$user/$filename" >/dev/null
+        sudo chown "$user:$user" "/home/$user/$filename"
+        log::info "Keypair written and ownership set for /home/$user/$filename"
+    done
 }
 
 apt::abk() {
-  $APT update
-  $APT install curl gnupg
-  if ! grep -q "^deb .*/zuma dev main" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    curl -s https://apt.abklabs.com/keys/abklabs-archive-dev.asc | $SUDO apt-key add -
-    echo "deb https://apt.abklabs.com/zuma dev main" | $SUDO tee /etc/apt/sources.list.d/zuma.list >/dev/null
     $APT update
-  fi
+    $APT install curl gnupg
+    if ! grep -q "^deb .*/zuma dev main" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+        curl -s https://apt.abklabs.com/keys/abklabs-archive-dev.asc | $SUDO apt-key add -
+        echo "deb https://apt.abklabs.com/zuma dev main" | $SUDO tee /etc/apt/sources.list.d/zuma.list >/dev/null
+        $APT update
+    fi
 }
