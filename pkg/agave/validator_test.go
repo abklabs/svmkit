d6a07602fce4f0422cdd40cb53495bbddfd87230
Alexander Guy <alexander.guy@pm.me>
2024-11-07 00:32:53 -0500

    Flatten and refactor this API

--- pkg/agave/validator_test.go
+++ pkg/agave/validator_test.go
@@ -7,29 +7,15 @@ import (
 )
 
 func TestValidatorEnv(t *testing.T) {
-	{
-		v := ValidatorEnv{}
-
-		if v.ToString() != "" {
-			t.Error("empty validator did not return an empty stirng")
-		}
+	m := Metrics{
+		URL:      "noproto://nowhere",
+		Database: "nodb",
+		User:     "notauser",
+		Password: "notapassword",
 	}
 
-	{
-		m := Metrics{
-			URL:      "noproto://nowhere",
-			Database: "nodb",
-			User:     "notauser",
-			Password: "notapassword",
-		}
-
-		v := ValidatorEnv{
-			Metrics: &m,
-		}
-
-		if v.ToString() != `SOLANA_METRICS_CONFIG="host=noproto://nowhere,db=nodb,u=notauser,p=notapassword"` {
-			t.Error("validator env output didn't match expectations")
-		}
+	if m.String() != `host=noproto://nowhere,db=nodb,u=notauser,p=notapassword` {
+		t.Error("validator env output didn't match expectations")
 	}
 }
 
