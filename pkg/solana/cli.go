0288c390b27f4c8f2e8195a8f0b13df9b487cfde
Alexander Guy <alexander.guy@pm.me>
2024-11-11 16:32:55 -0500

    Add support for setting the Solana CLI's default keypair

--- pkg/solana/cli.go
+++ pkg/solana/cli.go
@@ -5,13 +5,15 @@ import (
 )
 
 type CLIConfig struct {
-	URL *string
+	URL     *string
+	KeyPair *string
 }
 
 func (f *CLIConfig) ToFlags() *utils.FlagBuilder {
 	b := utils.FlagBuilder{}
 
 	b.AppendP("url", f.URL)
+	b.AppendP("keypair", f.KeyPair)
 
 	return &b
 }
