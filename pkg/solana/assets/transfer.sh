81bb7fddf9815dc9df4e65a8e30d7df04a46f4a1
Alexander Guy <alexander.guy@pm.me>
2024-11-13 19:58:21 -0500

    Add initial support for Transfers

---
+++ pkg/solana/assets/transfer.sh
@@ -0,0 +1,30 @@
+# -*- mode: shell-script -*-
+# shellcheck shell=bash
+
+umask 077
+
+transfer-create() {
+    local payer_keypair args
+
+    payer_keypair=$(temp::file)
+    echo "$PAYER_KEYPAIR" >"$payer_keypair"
+
+    args=()
+
+    if [[ -v ALLOW_UNFUNDED_RECIPIENT ]]; then
+        args+=(--allow-unfunded-recipient)
+    fi
+
+    cat "$payer_keypair"
+
+    solana -k "$payer_keypair" transfer "${args[@]}" "$RECIPIENT_PUBKEY" "$AMOUNT"
+}
+
+case "$TRANSFER_ACTION" in
+CREATE)
+    transfer-create
+    ;;
+*)
+    log::fatal "unknown action provided '$TRANSFER_ACTION'!"
+    ;;
+esac
