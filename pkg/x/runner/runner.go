ccef51acfb6ff5bfbbb5b9b835d6c93ac47013fb
Alexander Guy <alexander.guy@pm.me>
2024-11-17 16:21:50 -0500

    Implement the start of a new Runner
    This new implementation makes some room for API
    changes that tighten up how we deploy data to the
    remote machine, and make things simpler to implement.

---
+++ pkg/x/runner/runner.go
@@ -0,0 +1,51 @@
+package runner
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+
+	"github.com/abklabs/svmkit/pkg/runner"
+	"github.com/abklabs/svmkit/pkg/utils"
+	"golang.org/x/crypto/ssh"
+)
+
+type Command interface {
+	Check() error
+	Env() *utils.EnvBuilder
+	Script() string
+}
+
+func NewRunner(client *ssh.Client, cmd Command) *Runner {
+	return &Runner{client: client, command: cmd}
+}
+
+type Runner struct {
+	client  *ssh.Client
+	command Command
+}
+
+func (r *Runner) Run(ctx context.Context, handler DeployerHandler) error {
+	p := &Payload{
+		RootPath: fmt.Sprintf("/tmp/runner-%d-%d", time.Now().Unix(), rand.Int()),
+	}
+
+	p.AddString("lib.bash", runner.LibBash)
+	p.Add(PayloadFile{"run.sh", strings.NewReader(runner.RunScript), 0755})
+	p.AddReader("env", r.command.Env().Buffer())
+	p.AddString("steps.sh", r.command.Script())
+
+	d := Deployer{Payload: p, Client: r.client}
+
+	if err := d.Deploy(); err != nil {
+		return err
+	}
+
+	if err := d.Run([]string{"./run.sh"}, false, handler); err != nil {
+		return err
+	}
+
+	return nil
+}
