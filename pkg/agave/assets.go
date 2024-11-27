d682a68b93ba0422345399ace574c0cf0e9943a1
Alexander Guy <alexander.guy@pm.me>
2024-11-27 16:21:40 -0500

    Move Agave's check-validator to a text/template

--- pkg/agave/assets.go
+++ pkg/agave/assets.go
@@ -9,3 +9,4 @@ import (
 var assets embed.FS
 
 var installScriptTmpl = template.Must(template.ParseFS(assets, "assets/install.sh.tmpl"))
+var checkValidatorScriptTmpl = template.Must(template.ParseFS(assets, "assets/check-validator.tmpl"))
