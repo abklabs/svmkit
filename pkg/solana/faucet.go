1081a8e8be5ea264e3ff62b1481e2e96bf36fa67
Julian Lupu <monological@gmail.com>
2024-12-10 11:17:40 -0800

    Hard code faucet keypair path and expose it to Pulumi env

--- pkg/solana/faucet.go
+++ pkg/solana/faucet.go
@@ -2,8 +2,12 @@ package solana
 
 import (
 	"strings"
-	
+
 	"github.com/abklabs/svmkit/pkg/runner"
+)
+
+const (
+	faucetKeyPairPath = "/home/sol/faucet-keypair.json"
 )
 
 type InstallCommand struct {
@@ -18,9 +22,12 @@ func (cmd *InstallCommand) Env() *runner.EnvBuilder {
 
 	b.SetMap(map[string]string{
 		"FAUCET_FLAGS": strings.Join(cmd.Flags.ToArgs(), " "),
-		"FAUCET_PORT": "9900", // hardcoded in solana-faucet
-		"FAUCET_ENV": faucetEnv.String(),
+		"FAUCET_ENV":   faucetEnv.String(),
 	})
+
+	b.Set("FAUCET_PORT", "9900") // hardcoded in solana-faucet
+
+	b.SetP("FAUCET_VERSION", cmd.Version)
 
 	return b
 
@@ -38,16 +45,19 @@ func (cmd *InstallCommand) AddToPayload(p *runner.Payload) error {
 	}
 
 	p.AddReader("steps.sh", faucetScript)
+	p.AddString("faucet-keypair.json", cmd.KeyPair)
 
 	return nil
 }
 
 type Faucet struct {
-	Flags         FaucetFlags        `pulumi:"flags"`
+	Flags   FaucetFlags `pulumi:"flags"`
+	Version *string     `pulumi:"version,optional"`
+	KeyPair string      `pulumi:"keypair" provider:"secret"`
 }
 
 func (f *Faucet) ToArgs() []string {
-    return f.Flags.ToArgs()
+	return f.Flags.ToArgs()
 }
 
 func (f *Faucet) Install() runner.Command {
@@ -57,31 +67,27 @@ func (f *Faucet) Install() runner.Command {
 }
 
 type FaucetFlags struct {
-    // Required. Path to the keypair file used by the faucet.
-    KeypairPath string `pulumi:"keypairPath"`
+	// Optional. Allow requests from specified IPs without request limit.
+	// If multiple --allow-ip flags are provided, all specified IPs are allowed.
+	AllowIPs *[]string `pulumi:"allowIPs,optional"`
 
-    // Optional. Allow requests from specified IPs without request limit.
-    // If multiple --allow-ip flags are provided, all specified IPs are allowed.
-    AllowIPs *[]string `pulumi:"allowIPs,optional"`
+	// Optional. Request limit for a single request, in SOL.
+	// If not specified, no limit is applied.
+	PerRequestCap *int `pulumi:"perRequestCap,optional"`
 
-    // Optional. Request limit for a single request, in SOL.
-    // If not specified, no limit is applied.
-    PerRequestCap *int `pulumi:"perRequestCap,optional"`
+	// Optional. Request limit for a given time slice, in SOL.
+	// If not specified, no limit is applied.
+	PerTimeCap *int `pulumi:"perTimeCap,optional"`
 
-    // Optional. Request limit for a given time slice, in SOL.
-    // If not specified, no limit is applied.
-    PerTimeCap *int `pulumi:"perTimeCap,optional"`
-
-    // Optional. Length of the time slice in seconds.
-    // If not specified, no slicing is applied.
-    SliceSeconds *int `pulumi:"sliceSeconds,optional"`
+	// Optional. Length of the time slice in seconds.
+	// If not specified, no slicing is applied.
+	SliceSeconds *int `pulumi:"sliceSeconds,optional"`
 }
-
 
 func (f *FaucetFlags) ToArgs() []string {
 	b := runner.FlagBuilder{}
 
-	b.Append("--keypair", f.KeypairPath)
+	b.Append("--keypair", faucetKeyPairPath)
 
 	if f.AllowIPs != nil {
 		for _, ip := range *f.AllowIPs {
@@ -95,5 +101,3 @@ func (f *FaucetFlags) ToArgs() []string {
 
 	return b.ToArgs()
 }
-
-
