b653b84fed0634917421e6c23a729064debdb3e6
Alexander Guy <alexander.guy@pm.me>
2025-01-16 15:28:00 +0000

    Add some interfaces to start tracking deb information

---
+++ pkg/deb/package.go
@@ -0,0 +1,50 @@
+package deb
+
+type Package struct {
+	Name          string
+	Version       *string
+	TargetRelease *string
+}
+
+func (p *Package) String() string {
+	if p.Version != nil {
+		return p.Name + "=" + *p.Version
+	}
+
+	if p.TargetRelease != nil {
+		return p.Name + "/" + *p.TargetRelease
+	}
+
+	return p.Name
+}
+
+func (p Package) MakePackageGroup(names ...string) PackageGroup {
+	pkgs := make(PackageGroup, len(names))
+
+	for i, name := range names {
+		pkgs[i] = p
+		pkgs[i].Name = name
+	}
+
+	return pkgs
+}
+
+type PackageGroup []Package
+
+func (p PackageGroup) Args() []string {
+	ret := make([]string, len(p))
+
+	for i, v := range p {
+		ret[i] = v.String()
+	}
+
+	return ret
+}
+
+func (p *PackageGroup) Add(rest ...Package) {
+	*p = append(*p, rest...)
+}
+
+func NewPackageGroup(rest ...Package) PackageGroup {
+	return PackageGroup(rest)
+}
