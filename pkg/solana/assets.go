e5b73dd905915e266291860db5199cfd504c7a89
Alexander Guy <alexander.guy@pm.me>
2025-01-26 19:07:15 -0800

    Genesis -> solana/genesis

--- pkg/solana/assets.go
+++ pkg/solana/assets.go
@@ -8,7 +8,6 @@ import (
 var assets embed.FS
 
 const (
-	assetsGenesisScript      = "assets/genesis.sh"
 	assetsStakeAccountScript = "assets/stake-account.sh"
 	assetsTransferScript     = "assets/transfer.sh"
 	assetsVoteAccountScript  = "assets/vote-account.sh"
--- pkg/solana/genesis/genesis_test.go
+++ pkg/solana/genesis/genesis_test.go
@@ -1,4 +1,4 @@
-package solana
+package genesis
 
 import (
 	"testing"
