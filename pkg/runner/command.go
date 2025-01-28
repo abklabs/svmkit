4ce6c09b00e30f0a4413caf04bba031c4acb921e
Julian Lupu <monological@gmail.com>
2025-01-28 09:17:32 -0800

    Update runner to prevent apt-get lock contention

--- pkg/runner/command.go
+++ pkg/runner/command.go
@@ -10,6 +10,15 @@ type RunnerCommand struct {
 	packageGroup *deb.PackageGroup
 }
 
+func (r *RunnerCommand) SetConfigDefaults() {
+	if r.RunnerConfig != nil {
+		r.RunnerConfig.SetDefaults()
+	} else {
+		r.RunnerConfig = &Config{}
+		r.RunnerConfig.SetDefaults()
+	}
+}
+
 func (r *RunnerCommand) UpdatePackageGroup(grp *deb.PackageGroup) error {
 	if r.RunnerConfig != nil {
 		if err := r.RunnerConfig.UpdatePackageGroup(grp); err != nil {
@@ -31,11 +40,22 @@ func (r *RunnerCommand) AddToPayload(p *Payload) error {
 }
 
 func (r *RunnerCommand) Env() *EnvBuilder {
+	if r.RunnerConfig == nil {
+		panic("environment cannot be configured if the runner config's defaults haven't been set!")
+	}
+
 	if r.packageGroup == nil {
 		panic("environment cannot be configured if the package group hasn't been updated!")
 	}
 
 	env := NewEnvBuilder()
 	env.SetArray("PACKAGE_LIST", r.packageGroup.Args())
+
+	if r.RunnerConfig.AptLockTimeout != nil {
+		env.SetInt("APT_LOCK_TIMEOUT", *r.RunnerConfig.AptLockTimeout)
+	} else {
+		env.SetInt("APT_LOCK_TIMEOUT", aptLockTimeout)
+	}
+
 	return env
 }
--- pkg/runner/config.go
+++ pkg/runner/config.go
@@ -4,8 +4,20 @@ import (
 	"github.com/abklabs/svmkit/pkg/runner/deb"
 )
 
+const (
+	aptLockTimeout = 120
+)
+
 type Config struct {
-	PackageConfig *deb.PackageConfig `pulumi:"packageConfig,optional"`
+	PackageConfig  *deb.PackageConfig `pulumi:"packageConfig,optional"`
+	AptLockTimeout *int               `pulumi:"aptLockTimeout,optional"`
+}
+
+func (c *Config) SetDefaults() {
+	if c.AptLockTimeout == nil {
+		temp := aptLockTimeout
+		c.AptLockTimeout = &temp
+	}
 }
 
 func (c *Config) UpdatePackageGroup(grp *deb.PackageGroup) error {
