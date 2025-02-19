ee3b9d1a1010fd6d27027bc1b9f70857ac0edad4
Jacquin Mininger <jacquin.mininger@gmail.com>
2025-02-19 14:48:51 -0500

    feat: add deactivate/withdraw to stakeaccount

--- pkg/solana/assets/stake-account.sh
+++ pkg/solana/assets/stake-account.sh
@@ -8,11 +8,27 @@ stake-account-create () {
     solana delegate-stake "${SOLANA_CLI_TXN_FLAGS[@]}" stake_account.json vote_account.json
 }
 
+stake-account-deactivate () {
+    # TODO: Add optional stake authority flag
+    solana deactivate-stake "${SOLANA_CLI_TXN_FLAGS[@]}" stake_account.json
+}
+
+stake-account-withdraw () {
+    # TODO: Add a withdraw-authority flag + option
+    solana withdraw-stake "${SOLANA_CLI_TXN_FLAGS[@]}" stake_account.json  withdraw_account.json "$WITHDRAW_AMOUNT"
+}
+
 case "$STAKE_ACCOUNT_ACTION" in
     CREATE)
-	stake-account-create
-	;;
+        stake-account-create
+        ;;
+    DEACTIVATE)
+        stake-account-deactivate
+        ;;
+    WITHDRAW)
+        stake-account-withdraw
+        ;;
     *)
-	log::fatal "unknown action provided '$STAKE_ACCOUNT_ACTION'!"
+        log::fatal "unknown action provided '$STAKE_ACCOUNT_ACTION'!"
     ;;
 esac
--- pkg/solana/stakeaccount.go
+++ pkg/solana/stakeaccount.go
@@ -15,6 +15,12 @@ type StakeAccount struct {
 	Amount               float64              `pulumi:"amount"`
 }
 
+type WithdrawOptions struct {
+	WithdrawAccount string `pulumi:"withdrawAccount" provider:"secret"`
+	// Just withdraw full amount for now
+	// Amount          float64 `pulumi:"amount"`
+}
+
 func (v *StakeAccount) Create() runner.Command {
 	return &StakeAccountCreate{
 		StakeAccount: *v,
@@ -60,6 +66,90 @@ func (v *StakeAccountCreate) AddToPayload(p *runner.Payload) error {
 
 	p.AddString("stake_account.json", v.StakeAccountKeyPairs.StakeAccount)
 	p.AddString("vote_account.json", v.StakeAccountKeyPairs.VoteAccount)
+
+	if opt := v.TransactionOptions; opt != nil {
+		cli := CLITxnOptions{*opt}
+
+		err := cli.AddToPayload(p)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type StakeAccountUpdate struct {
+	StakeAccount
+}
+
+func (v *StakeAccountUpdate) Check() error {
+	return nil
+}
+
+func (v *StakeAccountUpdate) Env() *runner.EnvBuilder {
+	e := v.StakeAccount.Env()
+	e.Set("STAKE_ACCOUNT_ACTION", "DEACTIVATE")
+
+	return e
+}
+
+func (v *StakeAccountUpdate) AddToPayload(p *runner.Payload) error {
+	stakeAccountScript, err := assets.Open(assetsStakeAccountScript)
+
+	if err != nil {
+		return err
+	}
+
+	p.AddReader("steps.sh", stakeAccountScript)
+
+	p.AddString("stake_account.json", v.StakeAccount.StakeAccountKeyPairs.StakeAccount)
+
+	if opt := v.TransactionOptions; opt != nil {
+		cli := CLITxnOptions{*opt}
+
+		err := cli.AddToPayload(p)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type StakeAccountDelete struct {
+	StakeAccount
+	WithdrawOptions
+}
+
+func (v *StakeAccountDelete) Check() error {
+	//check that stake_amount >= withdraw amount
+	// Check that undelegated amount >= withdraw amount
+	return nil
+}
+
+func (v *StakeAccountDelete) Env() *runner.EnvBuilder {
+	e := v.StakeAccount.Env()
+	e.Set("STAKE_ACCOUNT_ACTION", "WITHDRAW")
+	e.SetFloat64("WITHDRAW_AMOUNT", v.StakeAccount.Amount)
+	// e.SetFloat64("WITHDRAW_AMOUNT", v.WithdrawOptions.Amount)
+
+	return e
+}
+
+func (v *StakeAccountDelete) AddToPayload(p *runner.Payload) error {
+	stakeAccountScript, err := assets.Open(assetsStakeAccountScript)
+
+	if err != nil {
+		return err
+	}
+
+	p.AddReader("steps.sh", stakeAccountScript)
+
+	p.AddString("stake_account.json", v.StakeAccount.StakeAccountKeyPairs.StakeAccount)
+	p.AddString("withdraw_account.json", v.WithdrawOptions.WithdrawAccount)
 
 	if opt := v.TransactionOptions; opt != nil {
 		cli := CLITxnOptions{*opt}
