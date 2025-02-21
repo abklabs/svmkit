60088622b34c74e3f805c27fcd2d14f693e26843
Jacquin Mininger <jacquin.mininger@gmail.com>
2025-02-21 16:30:01 -0500

    feat: add delete semantics to StakeAccount

--- pkg/solana/assets/stake-account.sh
+++ pkg/solana/assets/stake-account.sh
@@ -8,9 +8,23 @@ stake-account-create () {
     solana delegate-stake "${SOLANA_CLI_TXN_FLAGS[@]}" stake_account.json vote_account.json
 }
 
+stake-account-delete () {
+    [[ -v FORCE_DELETE ]] || return 0
+
+    local args=()
+    if [[ -v ADD_WITHDRAW_AUTHORITY ]]; then
+      args+=(--withdraw-authority withdraw_authority.json)
+    fi
+
+    solana withdraw "${SOLANA_CLI_TXN_FLAGS[@]}" "${args[@]}" stake_account.json $WITHDRAW_PUBKEY "$STAKE_AMOUNT"
+}
+
 case "$STAKE_ACCOUNT_ACTION" in
     CREATE)
 	stake-account-create
+	;;
+    DELETE)
+	stake-account-delete
 	;;
     *)
 	log::fatal "unknown action provided '$STAKE_ACCOUNT_ACTION'!"
--- pkg/solana/stakeaccount.go
+++ pkg/solana/stakeaccount.go
@@ -1,22 +1,34 @@
 package solana
 
 import (
+	"errors"
+
 	"github.com/abklabs/svmkit/pkg/runner"
 )
 
 type StakeAccountKeyPairs struct {
-	StakeAccount string `pulumi:"stakeAccount" provider:"secret"`
-	VoteAccount  string `pulumi:"voteAccount" provider:"secret"`
+	StakeAccount      string  `pulumi:"stakeAccount" provider:"secret"`
+	VoteAccount       string  `pulumi:"voteAccount" provider:"secret"`
+	StakeAuthority    *string `pulumi:"stakeAuthority,optional"`
+	WithdrawAuthority *string `pulumi:"withdrawAuthority,optional"`
 }
 
 type StakeAccount struct {
 	TransactionOptions   *TxnOptions          `pulumi:"transactionOptions"`
 	StakeAccountKeyPairs StakeAccountKeyPairs `pulumi:"keyPairs"`
 	Amount               float64              `pulumi:"amount"`
+	WithdrawAddress      *string              `pulumi:"withdrawAddress,optional"`
+	ForceDelete          bool                 `pulumi:"forceDelete"`
 }
 
 func (v *StakeAccount) Create() runner.Command {
 	return &StakeAccountCreate{
+		StakeAccount: *v,
+	}
+}
+
+func (v *StakeAccount) Delete() runner.Command {
+	return &StakeAccountDelete{
 		StakeAccount: *v,
 	}
 }
@@ -60,6 +72,60 @@ func (v *StakeAccountCreate) AddToPayload(p *runner.Payload) error {
 
 	p.AddString("stake_account.json", v.StakeAccountKeyPairs.StakeAccount)
 	p.AddString("vote_account.json", v.StakeAccountKeyPairs.VoteAccount)
+
+	if opt := v.TransactionOptions; opt != nil {
+		cli := CLITxnOptions{*opt}
+
+		err := cli.AddToPayload(p)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type StakeAccountDelete struct {
+	StakeAccount
+}
+
+func (v *StakeAccountDelete) Check() error {
+	if v.WithdrawAddress == nil && !v.ForceDelete {
+		return errors.New("must provide withdraw address or set force delete to true")
+	}
+	return nil
+}
+
+func (v *StakeAccountDelete) Env() *runner.EnvBuilder {
+	e := v.StakeAccount.Env()
+	e.Set("STAKE_ACCOUNT_ACTION", "DELETE")
+
+	if v.StakeAccount.StakeAccountKeyPairs.WithdrawAuthority != nil {
+		e.SetBool("ADD_WITHDRAW_AUTHORITY", true)
+	}
+
+	if v.ForceDelete {
+		e.SetBool("FORCE_DELETE", true)
+	}
+
+	return e
+}
+
+func (v *StakeAccountDelete) AddToPayload(p *runner.Payload) error {
+	stakeAccountScript, err := assets.Open(assetsStakeAccountScript)
+
+	if err != nil {
+		return err
+	}
+
+	p.AddReader("steps.sh", stakeAccountScript)
+
+	p.AddString("stake_account.json", v.StakeAccountKeyPairs.StakeAccount)
+
+	if v.StakeAccountKeyPairs.WithdrawAuthority != nil {
+		p.AddString("withdraw_authority.json", *v.StakeAccountKeyPairs.WithdrawAuthority)
+	}
 
 	if opt := v.TransactionOptions; opt != nil {
 		cli := CLITxnOptions{*opt}
