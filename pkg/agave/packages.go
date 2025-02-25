3c2d78140f574db184e9a0f15eda22df7f12d8fe
Alexander Guy <alexander.guy@pm.me>
2025-02-24 16:34:47 -0800

    Don't install or manage logrotate for Agave's log output
    Previously we had installed logrotate by default, to manage
    rotating Agave's log output.  As we are moving to using systemd
    journal by default, this is no longer required.

--- pkg/agave/packages.go
+++ pkg/agave/packages.go
@@ -33,7 +33,7 @@ func GeneratePackageInfo(variant *Variant, version *string) (*PackageInfo, error
 		return nil, err
 	}
 
-	info.PackageGroup = deb.Package{}.MakePackageGroup("ufw", "logrotate", "jq")
+	info.PackageGroup = deb.Package{}.MakePackageGroup("ufw", "jq")
 	info.PackageGroup.Add(deb.Package{Version: info.Version}.MakePackages("svmkit-solana-cli", info.Variant.PackageName())...)
 
 	return info, nil
