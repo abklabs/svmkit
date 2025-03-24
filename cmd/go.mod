4b3171d10251ee9628388bafaf9ea191406f3aa4
Alexander Guy <alexander.guy@pm.me>
2025-03-24 13:57:16 -0700

    Initial support for an svmkit CLI

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,3 +1,9 @@
 module github.com/abklabs/svmkit/cmd
 
 go 1.22.8
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+)
---
+++ cmd/svmkit/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "svmkit",
+		Short: "A CLI for interacting with svmkit",
+		Long:  `svmkit is a wrapper around the library of the same name.`,
+	}
+)
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
