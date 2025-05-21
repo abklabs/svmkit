08423e37691aa8fd6585c60f4fccb016a91adfd7
raykast <me@june-s.net>
2025-05-21 16:15:08 -0700

    Implement firedancer.Variant to configure FD fork.

--- pkg/firedancer/variant.go
+++ pkg/firedancer/variant.go
@@ -1,6 +1,8 @@
 package firedancer
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-go-provider/infer"
 )
 
@@ -23,5 +25,37 @@ func (Variant) Values() []infer.EnumValue[Variant] {
 			Value:       VariantFiredancer,
 			Description: "The Firedancer variant",
 		},
+	}
+}
+
+func (v Variant) Check() error {
+	switch v {
+	case VariantFrankendancer, VariantFiredancer:
+	default:
+		return fmt.Errorf("unknown validator variant '%s'", v)
+	}
+
+	return nil
+}
+
+func (v Variant) PackageName() string {
+	switch v {
+	case VariantFrankendancer:
+		return "svmkit-frankendancer"
+	case VariantFiredancer:
+		return "svmkit-firedancer"
+	default:
+		// XXX - mirroring behavior of agave/variant.go
+		return ""
+	}
+}
+
+func (v Variant) ServiceName() string {
+	switch v {
+	case VariantFrankendancer, VariantFiredancer:
+		return "svmkit-fd-validator"
+	default:
+		// XXX - mirroring behavior of agave/variant.go
+		return ""
 	}
 }
