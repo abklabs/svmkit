46d6a461e38e7480c803f027118dabee62e53528
raykast <me@june-s.net>
2025-06-02 14:21:04 -0700

    Add API for handling deletion of managed paths.

---
+++ pkg/deletion/assets.go
@@ -0,0 +1,25 @@
+package deletion
+
+import (
+	"embed"
+
+	"github.com/abklabs/svmkit/pkg/runner"
+)
+
+//go:embed assets
+var assets embed.FS
+
+const (
+	assetsLib = "assets/lib.sh"
+)
+
+func AddToPayload(p *runner.Payload) error {
+	file, err := assets.Open(assetsLib)
+	if  err != nil {
+		return err
+	}
+
+	p.AddReader("deletion-lib.sh", file)
+
+	return nil
+}
